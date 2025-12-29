544b42f4873059c7adca6f704d2d6b31d71af5c1
SKiRa2013 <skira250128@gmail.com>
2025-12-29 03:36:33 -0500

    2025-12-29 03:36,-05:00. Implementation of algoritms shown in 'Search' Lecture in Harvard's CS50 AI 2024 course.

--- sbc-folder/src/factorial.pl
+++
@@ -1,7 +0,0 @@
-factorial(2, 2).
-
-factorial(Num, Fact) :-
-    Num > 2,
-    Aux1 is Num-1,
-    factorial(Aux1, FAux),
-    Fact is Num * FAux.
--- sbc-folder/src/potencia.pl
+++
@@ -1,7 +0,0 @@
-potencia(_, 0, 1) :- !.
-
-potencia(Base, Exp, Power) :-
-    Exp > 0,
-    E1 is Exp-1,
-    potencia(Base, E1, PAcum),
-    Power is PAcum * Base, !.
